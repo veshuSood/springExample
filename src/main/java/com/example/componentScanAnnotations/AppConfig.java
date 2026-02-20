8b1fcbf2cc3e4cb9fc397f7effadcac382073ae1
blue <jatinsood60@gmail.com>
2026-02-20 19:08:55 +0530

    Component-Scan via annotations (no-xml)

---
+++ src/main/java/com/example/componentScanAnnotations/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.componentScanAnnotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.componentScanAnnotations")
+public class AppConfig {
+
+}
